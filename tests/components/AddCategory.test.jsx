3992203897ddf8bd8c3a96e97e56cc3c205cbfa8
CESCO <cescovalledev@gmail.com>
2023-05-29 22:10:37 -0600

    AddCategory onSubmit testing

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -10,4 +10,16 @@ describe("Pruebas en <AddCategory/>", () => {
 
     expect(input.value).toBe("Bloodborne");
   });
+
+  test("debe de llamar onNewCategory si el input tiene un valor", () => {
+    const inputValue = "Bloodborne";
+    render(<AddCategory onNewCategory={() => {}} />);
+
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+    expect(input.value).toBe("");
+  });
 });
